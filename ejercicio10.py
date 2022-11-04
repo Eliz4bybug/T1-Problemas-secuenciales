f3e27140194e303d027582556833a31c923ed05c
Elizabeth <eliz4.frivas@gmail.com>
2022-11-03 20:00:38 -0500

    correcciones del profesor

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,12 +1,24 @@
 import os
 os.system("cls")
-#entrada
+#entradax
 numero = int(input ("ingrese un numero natural de 4 cifras: "))
 
 #proceso
-print(numero)
-print()
-numero = str (numero)[::-1]
 
-#salida
-print(numero)
+'''' 12 // 10   C-> 1 
+12 % 10   R->2
+
+123 // 100
+
+1234 '''
+
+c4 = numero // 1000
+c4r = numero % 1000
+
+c3 = int((numero % 100) // 10)
+c2 = int((numero % 1000)// 100)
+c1 = 
+print(c4)
+print(c4r)
+print(c3)
+print(c2)
