d5b9962b2cf92340f616beaf643e0de2c7e5454b
Elizabeth <eliz4.frivas@gmail.com>
2022-11-06 15:33:23 -0500

    corección del ejercicio 9, se sube el ejercicio 11

--- ejercicio09.py
+++ ejercicio09.py
@@ -1,15 +1,18 @@
 import os
 os.system("cls")
 
-#falta acomodar y anotar
-n = int(input("Ingresa un numero: "))
+#entrada
+numero = int(input ("ingrese un numero natural de 4 cifras: "))
 
-suma = 0
+#proceso
+c4 = numero % 10
+c3 = int((numero % 100) / 10)
+c2 = int((numero % 1000) / 100)
+c1 = int((numero - (numero % 1000)) / 1000)
 
-while n > 0:
+sumatoria = c4 + c3 + c2 + c1
 
-    suma = suma + (n % 10)
-
-    n = n // 10
-
-print(f"La suma de los dígitos es: {suma: .2f}")
+#Salida
+print ( f"============ HALLANDO LA SUMATORIA DE LAS CIFRAS DE UN NÚMERO DE 4 DÍGITOS ===================")
+print(sumatoria)
+print(f"La Sumatoria de {numero: .2f} es: {sumatoria: .2f}")
--- ejercicio10.py
+++ ejercicio10.py
@@ -4,21 +4,21 @@ os.system("cls")
 numero = int(input ("ingrese un numero natural de 4 cifras: "))
 
 #proceso
+c4 = numero % 10
+c3 = int((numero % 100) / 10)
+c2 = int((numero % 1000) / 100)
+c1 = int((numero - (numero % 1000)) / 1000)
 
-'''' 12 // 10   C-> 1 
+''' 12 // 10   C-> 1 
 12 % 10   R->2
 
 123 // 100
 
-1234 '''
+1234 ''' 
 
-c4 = numero // 1000
-c4r = numero % 1000
-
-c3 = int((numero % 100) // 10)
-c2 = int((numero % 1000)// 100)
-c1 = 
+#Salida
 print(c4)
-print(c4r)
 print(c3)
 print(c2)
+print(c1)
+print(f"{c4}{c3}{c2}{c1}")
